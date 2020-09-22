bc458947b3036d966bbacf30ec778558383b3456
Oscar Martinez Ramos <oscar.martinezramos@outlook.com>
2020-09-22 11:30:47 +0200

    [feat] exercice 1

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -267,20 +267,4 @@ namespace GradeBook.GradeBooks
             return JsonConvert.DeserializeObject(json, gradebook);
         }
     }
-
-    public class StandardGradeBook : BaseGradeBook
-    {
-        public StandardGradeBook(string name) : base(name)
-        {
-            Type = GradeBookType.Standard;
-        }
-    }
-
-    public class RankedGradeBook : BaseGradeBook
-    {
-        public RankedGradeBook(string name) : base(name)
-        {
-            Type = GradeBookType.Ranked;
-        }
-    }
 }
